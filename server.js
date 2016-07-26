a799fbc425ed0efbf53de68e54389cb5bd03e983
Katie Vogel <katiebvogel@gmail.com>
2016-07-26 17:37:40 -0500

    got the project working!  There are unneccessary files and scripts sourced

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var index = require('./routes/index');
+var index = require('./index');
 var passport = require('passport');
 var session = require('express-session');
 var localStrategy = require('passport-local').Strategy;
 var mongoose = require('mongoose');
 var User = require('./models/user');
+var register = require('./routes/register');
+var login = require('./routes/login');
+
 
 var app = express();
 var mongoURI = "mongodb://localhost:27017/prime_example_passport";
@@ -31,20 +34,57 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(bodyParser.json());
+//We will require the url encoded body parser because of the way the index.html form is taking in information and routing as an html endpoint
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/', index);
+
+app.use('/register', register);
+app.use('/login', login);
 
 app.use(express.static('public'));
 
 
-// Katie -- start here on Tuesday with the passport.serializeUser(function(user, done))
+//this is the rest of the function for authenticating users
+passport.serializeUser(function(user, done){
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done){
+  User.findById(id, function(err, user){
+    if(err) {
+      return done(err);
+    }
+    done(null, user);
+  });
+});
 
 
 
-// passport.use('local', newlocalStrategy({ passReqToCallback: true}, usernameField: 'username' }, function(request, username, password, done) {
-//   //implementation will go here eventually
-//   console.log('we will add a function');
-// }
-// ));
+passport.use('local', new localStrategy({ passReqToCallback: true, usernameField: 'username' },
+function(request, username, password, done) {
+  User.findOne({ username: username}, function(err, user){
+    if(err) {
+      throw err
+    };
+    if(!user) {
+      return done(null, false, {message: 'Incorrect username and password.'});
+    }
+
+    //test a matching password
+    user.comparePassword(password, function(err, isMatch){
+      if (err) {
+        throw err;
+      }
+      if (isMatch) {
+        return done(null, user);
+      } else {
+        done(null, false, {message: 'Incorrect username and password.'});
+      }
+    }); //end user.comparePassword
+  }); //end User.findOne
+}) //end function including findOne
+);//end passport.use
 
 
 
